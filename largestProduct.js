c788045dbd320becf7c8c22f3f03c6db90b789db
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-09-21 10:05:51 -1000

    resolves all tests

--- largestProduct.js
+++ largestProduct.js
@@ -1,6 +1,26 @@
 class LargestProduct {
   constructor(number) {
+    let array = number.split("");
+    let index = 0;
+    let product = 0;
+    for (let i = 0; i < array.length - 4; i++) {
+      let challenger =
+        array[i] * array[i + 1] * array[i + 2] * array[i + 3] * array[i + 4];
+      if (challenger > product) {
+        product = challenger;
+        index = i;
+      }
+    }
+    this.index = index;
+    this.product = product;
+  }
 
+  getLargestProductIndex() {
+    return this.index;
+  }
+
+  getLargestProduct() {
+    return this.product;
   }
 }
 
